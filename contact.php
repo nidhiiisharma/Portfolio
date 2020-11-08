4f8e3b0e05aee7492b9b472119e77412dae1e661
Nidhi <nidhisharmas0103@gmail.com>
2020-11-09 00:26:59 +0100

    working on contact form

--- contact.php
+++ contact.php
@@ -13,7 +13,6 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
